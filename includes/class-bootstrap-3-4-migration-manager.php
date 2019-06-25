5c4b748f7240754c7a8ac6e446bf89e1fef091bf
Rayyan Albaz <rayyan95@my.yorku.ca>
2019-06-25 13:18:22 -0400

    update functions names to follow wp syntax

--- includes/class-bootstrap-3-4-migration-manager.php
+++ includes/class-bootstrap-3-4-migration-manager.php
@@ -1,37 +1,37 @@
 <?php
 class bootstrapMigration {
+
     public function __construct()
     {
-        $this->initializeTable();
+
     }
-    public function initializeTable(){
+    public function initialize_table(){
         global $wpdb;
-        $table_name = $wpdb->prefix.'bootstrapmigration';
+        $table_name = $wpdb->prefix.'bootstrap_dictionary';
         if($wpdb->get_var("SHOW TABLES LIKE '$table_name'") != $table_name) {
             //if table not in database. Create new table
             $charset_collate = $wpdb->get_charset_collate();
         
             $sql = "CREATE TABLE $table_name (
                 id mediumint(9) NOT NULL AUTO_INCREMENT,
-                field_x text NOT NULL,
-                field_y text NOT NULL,
+                old text NOT NULL,
+                new text NOT NULL,
+                created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
                 UNIQUE KEY id (id)
             ) $charset_collate;";
             require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
             dbDelta( $sql );
         }
-
     }
-    function deleteTable(){
+    function delete_table(){
         global $wpdb;
-        $table_name = $wpdb->prefix.'bootstrapmigration';
+        $table_name = $wpdb->prefix.'bootstrap_dictionary';
         if($wpdb->get_var("SHOW TABLES LIKE '$table_name'") == $table_name) {
             //if table in database. Delete table.
             global $wpdb;
             $sql = "DROP TABLE IF EXISTS $table_name;";
             $wpdb->query($sql);
         }
-
     }
 }
-$invoke = new bootstrapMigration();
+
