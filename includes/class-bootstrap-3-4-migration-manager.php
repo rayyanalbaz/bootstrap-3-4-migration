db0a4317c3edbe12156d35ec222ef4c875655222
Rayyan Albaz <rayyan95@my.yorku.ca>
2019-06-27 12:42:26 -0400

    Add update_dictionary function for database table

--- includes/class-bootstrap-3-4-migration-manager.php
+++ includes/class-bootstrap-3-4-migration-manager.php
@@ -3,6 +3,7 @@ class bootstrapMigration {
 
     public function __construct()
     {
+        
     }
     function initialize_table(){
         global $wpdb;
@@ -13,9 +14,12 @@ class bootstrapMigration {
         
             $sql = "CREATE TABLE $table_name (
                 id mediumint(9) NOT NULL AUTO_INCREMENT,
-                old text NOT NULL,
-                new text NOT NULL,
-                created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                old varchar(100) NOT NULL,
+                new varchar(100) NOT NULL,
+                created varchar(50) NOT NULL,
+                last_modified varchar(50) NOT NULL,
+                created_by varchar(50) NOT NULL,
+                updated_by varchar(50),
                 UNIQUE KEY id (id)
             ) $charset_collate;";
             require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
@@ -33,14 +37,16 @@ class bootstrapMigration {
         }
     }
     function add_dictionary($old,$new){
+        require_once( ABSPATH . 'wp-includes/pluggable.php' );
         global $wpdb;
         $table_name = $wpdb->prefix.'bootstrap_dictionary';
         $time = time();
+        $current_user = wp_get_current_user();
 
         $result = $wpdb->query("SELECT id FROM $table_name WHERE old = '$old'");
         if($result == 0) {
-            $sql = "INSERT INTO $table_name ( old, new, created)
-            VALUES ( '$old', '$new', '$time');";
+            $sql = "INSERT INTO $table_name ( old, new, created, last_modified, created_by )
+            VALUES ( '$old', '$new', '$time', '$time', '$current_user->user_login');";
             $wpdb->query($sql);
         }
     }
@@ -55,5 +61,18 @@ class bootstrapMigration {
         $table_name = $wpdb->prefix.'bootstrap_dictionary';
         $result =  $wpdb->get_row("SELECT * FROM $table_name WHERE old = '$old'", ARRAY_A , 0);
         return $result;
+    }
+    function update_dictionary($id, $old, $new){
+        require_once( ABSPATH . 'wp-includes/pluggable.php' );
+        global $wpdb;
+        $table_name = $wpdb->prefix.'bootstrap_dictionary';
+        $time = time();
+        $current_user = wp_get_current_user();
+
+        $sql = "UPDATE $table_name
+        SET old = '$old', new= '$new', last_modified = '$time', updated_by = '$current_user->user_login'
+        WHERE id = '$id';";
+        $wpdb->query($sql);
+
     }
 }
